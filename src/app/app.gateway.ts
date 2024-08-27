6f30170a64f2f10b401b43c894540d5fb46f3d8e
Копич Захар Сенадович <kopichzs@admlr.lipetsk.ru>
2024-08-27 15:37:58 +0300

    feat: AppGateway; chat.entity; app.service; app.controller

---
+++ src/app/app.gateway.ts
@@ -0,0 +1,41 @@
+import {
+  SubscribeMessage,
+  WebSocketGateway,
+  OnGatewayInit,
+  WebSocketServer,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+import { AppService } from '../app.service';
+import { Chat } from 'src/entities/chat.entity';
+
+@WebSocketGateway({})
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  constructor(private appService: AppService) {}
+
+  @WebSocketServer() server: Server;
+
+  @SubscribeMessage('sendMessage')
+  async handleSendMessage(client: Socket, payload: Chat): Promise<void> {
+    await this.appService.createMessage(payload);
+    this.server.emit('recMessage', payload);
+  }
+
+  afterInit(server: Server) {
+    console.log(server);
+    //Выполняем действия
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Disconnected: ${client.id}`);
+    //Выполняем действия
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.log(`Connected ${client.id}`);
+    //Выполняем действия
+  }
+}
